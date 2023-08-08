3252703ff9ff0d36fb66cb4d206600465165387a
GaburaisuVGC <matthieu.barbe01@gmail.com>
2023-08-08 09:38:57 +0200

    Fixed logout

--- vgc-hub/src/components/EmailVerificationError.js
+++ vgc-hub/src/components/EmailVerificationError.js
@@ -13,11 +13,22 @@ const EmailVerificationError = () => {
     }
   }, [navigate]);
 
+  const handleLogout = () => {
+    localStorage.clear();
+    navigate("/login");
+  };
+
   return (
     <div className="w-64 mx-auto mt-4 text-center">
       <h2 className="text-2xl font-bold mb-4">Email Verification Error</h2>
       <p className="mb-4">You are not verified. Please check your email or</p>
       <EmailResendForm />
+      <button
+                onClick={handleLogout}
+                className=" w-full px-4 py-2 mt-4 text-sm font-medium text-white bg-red-600 rounded-md hover:bg-red-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+              >
+                Déconnexion
+              </button>
     </div>
   );
 };
